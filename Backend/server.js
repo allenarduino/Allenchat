e0b8bcfc475aba3b5bdc227eb89947f51a6b39a2
ALLEN <aljay3334@gmail.com>
2021-09-11 14:59:45 +0100

    Changing the backend from PHP to Node

---
+++ Backend/server.js
@@ -0,0 +1,31 @@
+//importing dependencies
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+
+//importing routers
+const userRoute = require("./routes/userRoute");
+const messageRoute = require("./routes/messageRoute");
+
+// Function to serve all static files
+app.use("/uploads/", express.static("uploads"));
+
+//calling the dependencies
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+
+//calling the routers
+
+app.use(userRoute);
+app.use(messageRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function() {
+  console.log(`Server running on port:${PORT}`);
+});
